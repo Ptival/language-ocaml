c59aa27919d4b5bc91859cd08240685a52a9cd03
Valentin Robert <valentin.robert.42@gmail.com>
2018-05-14 15:53:49 -0700

    exposing pretty from PrettyPrinter.hs

--- lib/Language/OCaml/Parser.hs
+++ lib/Language/OCaml/Parser.hs
@@ -1,5 +1,13 @@
 module Language.OCaml.Parser
   ( implementation_P
+  , parseImplementation
   ) where
 
+import Text.Megaparsec (parse)
+import Text.Megaparsec.Error
+
+import Language.OCaml.Definitions.Parsing.ParseTree
 import Language.OCaml.Parser.Implementation
+
+parseImplementation :: String -> Either (ParseError Char Dec) Structure
+parseImplementation = parse implementation_P "Language.OCaml.Parser"
--- lib/Language/OCaml/PrettyPrinter.hs
+++ lib/Language/OCaml/PrettyPrinter.hs
@@ -1,5 +1,7 @@
 module Language.OCaml.PrettyPrinter
-  (
+  ( pretty
   ) where
+
+import Data.Text.Prettyprint.Doc (pretty)
 
 import Language.OCaml.PrettyPrinter.StructureItem ()
