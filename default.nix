7bebef52ab26e919bde85b05afeae45b91c786e9
Valentin Robert <valentin.robert.42@gmail.com>
2020-08-02 09:43:10 +0200

    attempt at using haskell.nix

--- default.nix
+++ default.nix
@@ -33,7 +33,7 @@ let
 
 in
 
-set.hsPkgs.${name}.components.exes // {
+set.hsPkgs.${name}.components.library // {
 
   shell = set.hsPkgs.shellFor {
 
@@ -41,28 +41,22 @@ set.hsPkgs.${name}.components.exes // {
       let
         hsPkgs = pkgs.haskell.packages.${compiler-nix-name};
       in [
-        # packages that must be installed for development
-        hsPkgs.haskell-language-server
+        # hsPkgs.haskell-language-server
       ];
 
     exactDeps = true;
 
     packages = p: [
-      # packages whose dependencies you need in scope
       p.language-ocaml
     ];
 
-    shellHook = ''
-       export HIE_HOOGLE_DATABASE=$(realpath "$(dirname "$(realpath "$(which hoogle)")")/../share/doc/hoogle/default.hoo")
-     '';
-
     tools = {
-      alex = "3.2.5";
-      cabal = "3.2.0.0";
-      happy = "1.19.12";
-      hlint = "2.2.11";
-      hpack = "0.34.2";
-      ormolu = "0.1.2.0";
+      # alex = "3.2.5";
+      # cabal = "3.2.0.0";
+      # happy = "1.19.12";
+      # hlint = "2.2.11";
+      # hpack = "0.34.2";
+      # ormolu = "0.1.2.0";
     };
 
   };
